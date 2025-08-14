49075e76369d2a1f4facadf519a05bf40ed2f050
Roman Novikov <58274176+nolikoff@users.noreply.github.com>
2025-08-14 05:00:47 +0300

    Update settingMetadataIds.ts

--- src/metadataHelpers/settingMetadataIds.ts
+++ src/metadataHelpers/settingMetadataIds.ts
@@ -3,10 +3,12 @@ export type SettingMetadataId =
   | "bar-at-top"
   | "name-tags"
   | "show-bars"
-  | "segments";
+  | "teamSegments"
+  | "strangersSegments";
 
 export const OFFSET_METADATA_ID = "offset";
 export const BAR_AT_TOP_METADATA_ID = "bar-at-top";
 export const NAME_TAGS_METADATA_ID = "name-tags";
 export const SHOW_BARS_METADATA_ID = "show-bars";
-export const SEGMENTS_METADATA_ID = "segments";
+export const TEAM_SEGMENTS_METADATA_ID = "teamSegments";
+export const STRANGERS_SEGMENTS_METADATA_ID = "strangersSegments"
