a34aaa9bcb128fb6d29bff0288ea3af9ea30114b
Roman Novikov <58274176+nolikoff@users.noreply.github.com>
2025-08-15 07:53:04 +0300

    Update Footer.tsx

--- src/action/Footer.tsx
+++ src/action/Footer.tsx
@@ -310,7 +310,10 @@ export default function Footer({
           <PopoverContent
             className="w-72 p-0"
             align="start"
-            style={{ height: playerRole === "GM" ? 440 : 245 }}
+            style={{ 
+              height: playerRole === "GM" ? 440 : 245,
+              width: playerRole === "GM" ? 328 : 328, 
+            }}
           >
             <ScrollArea className="h-full px-4">
               <div className="flex flex-col gap-2 py-3">
