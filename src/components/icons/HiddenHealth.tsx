53374e8066b11efd47777d5fa07edd2f873cbb57
Roman Novikov <58274176+nolikoff@users.noreply.github.com>
2025-08-15 15:24:57 +0300

    Update HiddenHealth.tsx

--- src/components/icons/HiddenHealth.tsx
+++ src/components/icons/HiddenHealth.tsx
@@ -1,8 +1,8 @@
 export const HiddenHealth = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
+    width="22"
+    height="22"
     viewBox="0 0 24 24"
     fill="none"
     className="lucide lucide-hidden-health"
