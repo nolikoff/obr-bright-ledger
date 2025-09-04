90f137d7859fdc829c68986499d6f9d4de091f26
Roman Novikov <58274176+nolikoff@users.noreply.github.com>
2025-09-05 00:28:51 +0300

    Delete src directory

--- src/metadataHelpers/sceneMetadataIds.ts
+++
@@ -1,9 +0,0 @@
-export type SceneMetadataId =
-  | "DICE-ROLLS"
-  | "MODE"
-  | "SHOW-ITEMS"
-;
-
-export const SCENE_DICE_ROLLS_METADATA_ID = "DICE-ROLLS";
-export const SCENE_MODE_METADATA_ID = "MODE";
-export const SCENE_SHOW_ITEMS_METADATA_ID = "MODE";
